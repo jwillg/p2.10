1663061053d818b4b749f8338e58ac43717341da
jwillg <jowillgrubs@gmail.com>
2019-12-08 16:52:25 -0600

    Update rules.js
    Co-Authored-By: charlottephillipss <charlottephillipss@users.noreply.github.com>
    Co-Authored-By: donaldsoundy <donald.soundy@ttu.edu>
    Co-Authored-By: Chloe Osgood <chloeosgood@users.noreply.github.com>

--- rules.js
+++ rules.js
@@ -1,35 +1,94 @@
-import Math
+var compChoice;
+var compare = "TEST TEXT";
+var playTier = 4;
+var compTier = 4;
+var TIER_TITLE = "Tier";
+var checkWin = false;
+var playerChoice = "Rock";
+var compChoiceN = 0;
+var tieBreaker = 0;
 
-var compChoice= Math.random() % 3;
-
-if (compChoice >= 1) {
-    compChoice = "Rock"
-}
-else if(compChoice >= 2) {
-    compChoice = "Paper"
-}
-else{
-    compChoice = "Scissors"
+function onLoad() {
+    document.getElementById("CTier").innerHTML = TIER_TITLE + " " + compTier;
+    document.getElementById("PTier").innerHTML = TIER_TITLE + " " + playTier;
+    document.getElementById("RoundText").innerHTML = "Text is here";
+    return 0;
 }
 
-var compare = function(choice1, choice2){
-    if(choice1 == choice2){
-        return "TIE";
-    }
-    if (choice1 == "Rock"){
-        if(choice2 == "Scissors"){
-            return "You win!";
+function startRound() {
+    // compChoiceN = Math.random() % 3;
+
+    let choiceArray = ["Rock", "Paper", "Scissors"];
+    let randomNum = Math.floor(Math.random()*choiceArray.length);
+    compChoice = choiceArray[randomNum];
+    console.log(compChoice);
+
+    decide(playerChoice, compChoice);
+
+    if (checkWin == true) {
+        if (playTier == 8) {
+            document.getElementById("RoundText").innerHTML = "Game over! You win!";
         }
-        else{
-            return "You lose!";
+        else if (playTier == 0) {
+            document.getElementById("RoundText").innerHTML = "Game over! You lose!";
         }
-    }
-    if (choice1 == "Scissors"){
-        if (choice2 == "Paper"){
-            return "You win!";
+        else if (compTier == 0) {
+            document.getElementById("RoundText").innerHTML = "Game over! You win!";
         }
         else {
-            return "You lose!";
+            document.getElementById("RoundText").innerHTML = "Game over! You lose!";
+        }
+    }
+
+    // decide(playerChoice, compChoice);
+
+    document.getElementById("RoundText").innerHTML = compare;
+    document.getElementById("CTier").innerHTML = TIER_TITLE + " " + compTier;
+    document.getElementById("PTier").innerHTML = TIER_TITLE + " " + playTier;
+
+    if (playTier == 0 || playTier == 8 || compTier == 0 || compTier == 8) {
+        checkWin = true;
+    }
+
+}
+
+function newGame() {
+    compChoice = 0;
+    compare = 0;
+    playTier = 4;
+    compTier = 4;
+}
+
+function decide(choice1, choice2) {
+
+    if (choice1.localeCompare(choice2)) {
+        console.log("Here");
+
+            compare = "TIE";
+    }
+
+    if (choice1.localeCompare("Rock")) {
+        if (choice2.localeCompare("Scissors")) {
+            compare = "Player wins this round!";
+            playTier++;
+            compTier--;
+        }
+
+        else {
+            compare = "Computer wins this round!";
+            compTier++;
+            playTier--;
+        }
+    }
+    if (choice1.localeCompare("Scissors")) {
+        if (choice2.localeCompare("Paper")) {
+            compare = "Player wins this round!";
+            playTier++;
+            compTier--;
+        } else {
+            compare = "Computer wins this round!";
+            playTier--;
+            compTier++;
         }
     }
 }
